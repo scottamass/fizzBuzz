c2de65671aeda06d2f6e18de3a619ee644357b9b
scottamass <scot.atkinson@live.co.uk>
2021-11-12 13:47:32 +0000

    added 7+11 and cleaned up code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,15 +6,30 @@ public class Main {
 
 
         for (int n = 1; n <= 100; n++) {
-            if (n % 15 == 0) {
-                System.out.println("fizzBuzz");
-            } else if (n % 3 == 0) {
-                System.out.println("fizz");
-            } else if (n % 5 == 0) {
-                System.out.println("BUZZ");
-            }else{
-                System.out.println(n);
+            String displayValue = "";
+            if (n % 3 == 0) {
+                displayValue += "fizz";
+                System.out.println(displayValue);
             }
+            else if (n % 5 == 0) {
+                displayValue += "BUZZ";
+                System.out.println(displayValue);
+            }
+            else if (n % 15 == 0) {
+                displayValue += "Fizz Buzz";
+                System.out.println(displayValue);
+            }
+            else if (n % 7 == 0) {
+                displayValue += "bong";
+                System.out.println(displayValue);
+            }
+            else if (n % 11 == 0) {
+                displayValue += "bang";
+                System.out.println(displayValue);
+            }
+
+            else System.out.println(n);
+
         }
     }
 }
